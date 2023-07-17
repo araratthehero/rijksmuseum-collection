c7517c527355c6367ef8aa784bf5ff5dde1ac7ac
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2023-07-17 21:07:56 +0200

    Add library versions and clean up the build.gradle file

--- app/src/main/java/com/mnatsakanyan/rijksmuseum/MainActivity.kt
+++ app/src/main/java/com/mnatsakanyan/rijksmuseum/MainActivity.kt
@@ -3,7 +3,9 @@ package com.mnatsakanyan.rijksmuseum
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.core.view.WindowCompat.setDecorFitsSystemWindows
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
