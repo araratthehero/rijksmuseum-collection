392facdc5a9189f7ff8d66dabaf831d87b6b1851
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2023-07-18 10:02:47 +0200

    Add art object repository logic

--- data/src/main/java/com/mnatsakanyan/data/repository/artobject/DefaultArtObjectRepository.kt
+++ data/src/main/java/com/mnatsakanyan/data/repository/artobject/DefaultArtObjectRepository.kt
@@ -1,15 +1,24 @@
 package com.mnatsakanyan.data.repository.artobject
 
 import com.mnatsakanyan.data.model.RequestedArtObject
+import com.mnatsakanyan.data.model.asExternalModel
+import com.mnatsakanyan.data.network.NetworkDataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class DefaultArtObjectRepository @Inject constructor(
+        private val networkDataSource: NetworkDataSource,
         private val dispatcher: CoroutineDispatcher
 ) : ArtObjectRepository {
-    // TODO: Implement the content here
-    override fun fetchArtObject(artObjectNumber: String): Flow<RequestedArtObject> {
-        TODO("Not yet implemented")
-    }
+
+    override fun fetchArtObject(artObjectNumber: String): Flow<RequestedArtObject> =
+            flow {
+                val artObject = networkDataSource.getArtObjectDetail(artObjectNumber)
+                        .asExternalModel()
+                emit(artObject)
+            }.flowOn(dispatcher)
 }
+
